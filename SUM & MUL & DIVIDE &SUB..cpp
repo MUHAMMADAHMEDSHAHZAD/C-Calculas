625300765e7ab5723d1197013f8bc3f46914a165
Muhammad Ahmed Shahzad <114295067+MUHAMMADAHMEDSHAHZAD@users.noreply.github.com>
2022-11-02 11:19:49 -0700

    Add files via upload

---
+++ SUM & MUL & DIVIDE &SUB..cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,sum,sub,m,d;
+	cout<<"enter 1 number";
+	cin>>a;
+	cout<<"enter 2 number";
+	cin>>b;
+	cout<<"enter 3 number";
+	cin>>c;
+	sum=a+b+c;
+	cout<<a<<endl<<b<<endl<<c<<endl<<sum<<endl;
+	sub=a-b-c;
+	cout<<a<<endl<<b<<endl<<c<<endl<<sub<<endl;
+	m=a*b*c;
+	cout<<a<<endl<<b<<endl<<c<<endl<<m<<endl;
+	d=a/b/c;
+	cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl;	
+}
